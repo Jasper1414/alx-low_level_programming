9b3f2cf6a6ae92d1206678aeac39fa118c805344
freedom <mphilelafreedom@gmail.com>
2023-08-14 15:35:47 -0700

    4-sum.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - prints the name of the file
  *   *
  *    * Return: Always 0 (Success)
  *     */
-int main(void)
+int ain(void)
 {
 		printf("%s\n", __FILE__);
 			return (0);
